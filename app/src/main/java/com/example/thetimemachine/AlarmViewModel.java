e3a741ad2dee16d4bd96fba53508da515b1a24fe
Shauleiz <shaul.eiz@gmail.com>
2023-10-21 19:51:23 +0300

    Time related project First step based on https://learntodroid.com/how-to-create-a-simple-alarm-clock-app-in-android/

---
+++ app/src/main/java/com/example/thetimemachine/AlarmViewModel.java
@@ -0,0 +1,7 @@
+package com.example.thetimemachine;
+
+import androidx.lifecycle.ViewModel;
+
+public class AlarmViewModel extends ViewModel {
+    public void NewAlarm(){};
+}
---
+++ app/src/main/java/com/example/thetimemachine/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.thetimemachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    // ViewModel object of class MyViewModel
+    // Holds all UI variables related to this activity
+    AlarmViewModel alarmViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create/acquire the ViewModel object of class AlarmViewModel
+        alarmViewModel = new ViewModelProvider(this).get(AlarmViewModel.class);
+
+        // Display the main Activity window
+        setContentView(R.layout.activity_main);
+
+        // Add alarm floating button
+        // Get it from the XML file then define what it does when clicked
+        FloatingActionButton AddAlarm_Button = findViewById(R.id.Add_Allarm_fab);
+        AddAlarm_Button.setOnClickListener(v -> alarmViewModel.NewAlarm());
+    }
+}
