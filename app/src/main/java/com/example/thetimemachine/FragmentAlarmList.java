ce10bcc51ed6607eaf67317e3380cfd8c205c519
Shauleiz <shaul.eiz@gmail.com>
2023-10-26 13:25:48 +0300

    Compilation bug fixed

--- app/src/main/java/com/example/thetimemachine/FragmentAlarmList.java
+++ app/src/main/java/com/example/thetimemachine/FragmentAlarmList.java
@@ -1,24 +1,12 @@
 package com.example.thetimemachine;
 
-import android.annotation.SuppressLint;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 
-import com.example.thetimemachine.databinding.FragmentAlarmListBinding;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
