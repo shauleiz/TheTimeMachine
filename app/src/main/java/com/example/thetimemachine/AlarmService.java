60e7022d44987e4f2a6b5d056ed04ef0ce1bb3e1
Shauleiz <shaul.eiz@gmail.com>
2023-12-14 19:30:41 +0200

    Alarm Service: Just added. Sets the notification but the notification does not pop-up.

--- app/src/main/java/com/example/thetimemachine/AlarmService.java
+++ app/src/main/java/com/example/thetimemachine/AlarmService.java
@@ -1,11 +1,12 @@
 package com.example.thetimemachine;
 
+import static com.example.thetimemachine.Application.TheTimeMachineApp.CHANNEL_ID;
+
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.VibrationEffect;
@@ -24,29 +25,43 @@ public class AlarmService  extends Service {
    @Override
    public int onStartCommand(Intent intent, int flags, int startId) {
 
+      Log.i("THE_TIME_MACHINE", "Service Started");
       // Display Activity: Stop, Snooze and general data
       // TODO: Define activity Intent notificationIntent = new Intent(this, RingActivity.class);
-      // PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+      //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
       // Display notification: With Text, Icon and option to call activity
-      /*
       String alarmTitle = String.format("Alarm:  %s", intent.getStringExtra("LABEL"));
+      // request code and flags not added for demo purposes
+      PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
       Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle(alarmTitle)
             .setContentText("Ring Ring .. Ring Ring")
-            .setSmallIcon(R.drawable.ic_alarm_black_24dp)
+            .setSmallIcon(R.drawable.baseline_alarm_add_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setFullScreenIntent(pendingIntent, true)
             .setContentIntent(pendingIntent)
             .build();
-      */
 
-      Log.i("THE_TIME_MACHINE", "Service Started");
+
+
+      Log.i("THE_TIME_MACHINE", alarmTitle);
+      // Display Notification
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+         startForeground( 1, notification, 0);
+      }
+      else
+         startForeground(1,notification);
+
+      Log.i("THE_TIME_MACHINE", "Service Started.2");
       // mediaPlayer.start();
-
+ /*
       if (Build.VERSION.SDK_INT >= 26) {
          ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(150,10));
       } else {
          ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(150);
       }
+
 
       Intent i = new Intent();
       i.setClass(this, MainActivity.class);
@@ -54,10 +69,9 @@ public class AlarmService  extends Service {
       i.addCategory(Intent.CATEGORY_LAUNCHER);
       i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       startActivity(i);
+       */
 
-
-      // Display Notification
-      //startForeground(1, notification);
+      Log.i("THE_TIME_MACHINE", "Service Started.3");
 
       return START_STICKY;
    }
@@ -65,7 +79,7 @@ public class AlarmService  extends Service {
    public void onCreate() {
       super.onCreate();
 
-      startForeground(1, null);
+      //startForeground(1, null);
       //mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
       //mediaPlayer.setLooping(true);
 
