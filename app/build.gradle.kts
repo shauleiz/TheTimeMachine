232725a824e04bba9a0a6be1e3ac99ebad7d9033
Shauleiz <shaul.eiz@gmail.com>
2024-04-05 19:18:11 +0300

    Release 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.thetimemachine"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1001
-        versionName = "1.0.2"
+        versionCode = 1002
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
