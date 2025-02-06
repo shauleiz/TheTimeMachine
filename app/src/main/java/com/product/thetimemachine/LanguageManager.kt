806ea65962b8cd83a2e1bce9c8f7d1b243599e5c
Shauleiz <shaul.eiz@gmail.com>
2025-02-06 09:26:18 +0200

    Internationalization (Locale)
    Preferences: On start-up application reads locale from preferences and sets to the correct locale
    If missing -use the default locale

--- app/src/main/java/com/product/thetimemachine/LanguageManager.kt
+++ app/src/main/java/com/product/thetimemachine/LanguageManager.kt
@@ -4,24 +4,27 @@ import android.content.Context
 import android.content.res.Configuration
 import android.util.Log
 import com.product.thetimemachine.ui.getPrefLanguage
+import java.util.IllformedLocaleException
 import java.util.Locale
 
 object LanguageManager {
-    private const val LANGUAGE_KEY = "language_key"
-    private const val DEFAULT_LANGUAGE = "en"
-
-    fun saveLanguage(context: Context, language: String) {
-        val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-        sharedPreferences.edit().putString(LANGUAGE_KEY, language).apply()
-    }
 
     fun getLanguage(context: Context): String {
         val language = getPrefLanguage(context)
-        return language
+        return ""//language
     }
 
     fun setLocale(context: Context, language: String): Context {
-        val locale = Locale(language)
+
+        if (language.isBlank())
+            return context
+
+        val locale = try {
+            Locale.Builder().setLanguage("").build()
+        } catch (e: IllformedLocaleException){
+            Locale.getDefault()
+        }
+
         Locale.setDefault(locale)
         val config = Configuration(context.resources.configuration)
         config.setLocale(locale)
