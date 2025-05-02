b86cb9d8b251fa4353bf1fac0e5620515d252b14
Shauleiz <Shaul.eiz@gmail.com>
2025-05-02 09:21:59 +0300

    Release 2.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.product.thetimemachine"
         minSdk = 27
         targetSdk = 34
-        versionCode = 20005
-        versionName = "2.0.5"
+        versionCode = 20006
+        versionName = "2.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
